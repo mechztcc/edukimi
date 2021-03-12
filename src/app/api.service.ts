9e7caae44905e711ab9bb826b00b99d82db5a2c1
mechztcc <dojathan@gmail.com>
2021-03-12 19:04:59 -0300

    Changes in the body size

---
+++ src/app/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+type User = {
+  id: number,
+  firstName: string,
+  secondName: string,
+  email: string,
+  password: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  
+
+  baseUrl: string = 'http://localhost:8080'
+
+  constructor(private http: HttpClient) { }
+
+  list(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/${id}`)
+  }
+}
